3cc2fac579b4eb986ec16e62933ac556265feee8
Mariama Tambedou <tambedou89mariama@gmail.com>
2023-09-10 20:57:56 +0000

    first commit

---
+++ src/main/java/com/example/crud/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.example.crud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long employeeid;
+    private String employeeName;
+    private float employeeSalary;
+
+    public Employee() {
+    }
+
+    public Employee(long employeeid, String employeeName, float employeeSalary) {
+        this.employeeid = employeeid;
+        this.employeeName = employeeName;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public long getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(long employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public float getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    public void setEmployeeSalary(float employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+}
